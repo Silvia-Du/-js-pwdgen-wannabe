185a4f57af27601d8a0edca6033f3a507f746676
Silvia-Du <silviadu@hotmail.it>
2022-04-05 14:48:11 +0200

    First commit: basic work setting

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+a- Chiedere nome all'utente
+b- Chiedere cognome all'utente
+c- Chiedre all'utente il suo colore preferito
+d- stampare in pagina: nomecognomecolorepreferito22
+
+Flusso di lavoro:
+1. inizializzare le variabili
+2. far inserire i dati
+3. Stampare tutto a video
+*/
+
